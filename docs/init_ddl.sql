27633296eac84a9ed3c13d2693f9aabc52338583
yangahh <toaur6802@naver.com>
2025-01-08 16:27:38 +0900

    docs: init ddl 에러 수정

--- docs/init_ddl.sql
+++ docs/init_ddl.sql
@@ -5,52 +5,22 @@ CREATE TABLE `users` (
                          `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
----
+--
 
-CREATE TABLE `queue` (
+CREATE TABLE `queue_token` (
                          `id` bigint PRIMARY KEY AUTO_INCREMENT,
                          `token_uuid` char(36) UNIQUE NOT NULL,
                          `user_id` bigint NOT NULL,     -- FK를 걸지 않음
                          `concert_id` bigint NOT NULL,  -- FK를 걸지 않음
-                         `status` enum NOT NULL,
+                         `is_active` boolean NOT NULL DEFAULT false,
                          `expired_at` timestamp NOT NULL,
                          `created_at` timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_queue_user_id ON queue(user_id);
-CREATE INDEX idx_queue_concert_id ON queue(concert_id);
+CREATE INDEX idx_queue_token_user_id ON queue_token(user_id);
+CREATE INDEX idx_queue_token_concert_id ON queue_token(concert_id);
 
----
-
-CREATE TABLE `point` (
-                         `id` bigint PRIMARY KEY AUTO_INCREMENT,
-                         `user_id` bigint NOT NULL,
-                         `balance` bigint NOT NULL DEFAULT 0,
-                         `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-                         `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-ALTER TABLE `point` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
----
-
-CREATE TABLE `point_history` (
-                                 `id` bigint PRIMARY KEY AUTO_INCREMENT,
-                                 `point_id` bigint NOT NULL,
-                                 `user_id` bigint NOT NULL,
-                                 `transaction_type` enum NOT NULL,
-                                 `amount` bigint NOT NULL,
-                                 `balance` bigint NOT NULL,
-                                 `ref_id` bigint,
-                                 `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-                                 `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-ALTER TABLE `point_history` ADD FOREIGN KEY (`ref_id`) REFERENCES `reservation` (`id`);
-ALTER TABLE `point_history` ADD FOREIGN KEY (`point_id`) REFERENCES `point` (`id`);
-ALTER TABLE `point_history` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
----
+--
 
 CREATE TABLE `concert` (
                            `id` bigint PRIMARY KEY AUTO_INCREMENT,
@@ -59,11 +29,11 @@ CREATE TABLE `concert` (
                            `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
----
+--
 
 CREATE TABLE `concert_schedule` (
                                     `id` bigint PRIMARY KEY AUTO_INCREMENT,
-                                    `concert_id` integer NOT NULL,
+                                    `concert_id` bigint NOT NULL,
                                     `event_date` datetime NOT NULL,
                                     `total_seat_count` integer NOT NULL DEFAULT 0,
                                     `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
@@ -72,11 +42,11 @@ CREATE TABLE `concert_schedule` (
 
 ALTER TABLE `concert_schedule` ADD FOREIGN KEY (`concert_id`) REFERENCES `concert` (`id`);
 
----
+--
 
 CREATE TABLE `seat` (
-                        `id` integer PRIMARY KEY AUTO_INCREMENT,
-                        `concert_schedule_id` integer NOT NULL,
+                        `id` bigint PRIMARY KEY AUTO_INCREMENT,
+                        `concert_schedule_id` bigint NOT NULL,
                         `seat_no` varchar(255) NOT NULL,
                         `is_available` boolean NOT NULL DEFAULT true,
                         `price` integer NOT NULL,
@@ -86,13 +56,13 @@ CREATE TABLE `seat` (
 
 ALTER TABLE `seat` ADD FOREIGN KEY (`concert_schedule_id`) REFERENCES `concert_schedule` (`id`);
 
----
+--
 
 CREATE TABLE `reservation` (
-                               `id` integer PRIMARY KEY AUTO_INCREMENT,
-                               `user_id` integer NOT NULL,
-                               `seat_id` integer NOT NULL,
-                               `status` enum NOT NULL DEFAULT 'PENDING_PAYMENT',
+                               `id` bigint PRIMARY KEY AUTO_INCREMENT,
+                               `user_id` bigint NOT NULL,
+                               `seat_id` bigint NOT NULL,
+                               `status` ENUM('PENDING_PAYMENT', 'CONFIRMED', 'CANCELED') NOT NULL DEFAULT 'PENDING_PAYMENT',
                                `payment_price` integer NOT NULL,
                                `temp_reservation_expired_at` timestamp NOT NULL,
                                `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
@@ -101,3 +71,33 @@ CREATE TABLE `reservation` (
 
 ALTER TABLE `reservation` ADD FOREIGN KEY (`seat_id`) REFERENCES `seat` (`id`);
 ALTER TABLE `reservation` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+
+CREATE TABLE `point` (
+                         `id` bigint PRIMARY KEY AUTO_INCREMENT,
+                         `user_id` bigint NOT NULL,
+                         `balance` integer NOT NULL DEFAULT 0,
+                         `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+                         `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+ALTER TABLE `point` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+
+CREATE TABLE `point_history` (
+                                 `id` bigint PRIMARY KEY AUTO_INCREMENT,
+                                 `point_id` bigint NOT NULL,
+                                 `user_id` bigint NOT NULL,
+                                 `transaction_type` ENUM('USE', 'CHARGE') NOT NULL,
+                                 `amount` integer NOT NULL,
+                                 `balance` integer NOT NULL,
+                                 `ref_id` bigint,
+                                 `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+                                 `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+ALTER TABLE `point_history` ADD FOREIGN KEY (`ref_id`) REFERENCES `reservation` (`id`);
+ALTER TABLE `point_history` ADD FOREIGN KEY (`point_id`) REFERENCES `point` (`id`);
+ALTER TABLE `point_history` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
