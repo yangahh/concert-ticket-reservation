e178b85ada8c69b6f48bdbca1691db6385a794d4
yangahh <toaur6802@naver.com>
2025-01-10 03:09:06 +0900

    chore: appliction properties file 분리, bean scan 버그 수정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -2,8 +2,10 @@ package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"kr.hhplus.be.server"})
 public class ServerApplication {
 
 	public static void main(String[] args) {
