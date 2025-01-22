d649b4eb56fecf6cb9172c53e38ef890a6a4b98f
yangahh <toaur6802@naver.com>
2025-01-23 01:18:45 +0900

    fix: 테스트코드 에러 수정

--- src/test/java/kr/hhplus/be/server/tests/reservation/integration/ReservationConcurrencyTest.java
+++ src/test/java/kr/hhplus/be/server/tests/reservation/integration/ReservationConcurrencyTest.java
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.tests.reservation.integration;
 
 import kr.hhplus.be.server.application.reservation.usecase.ReservationUseCase;
+import kr.hhplus.be.server.domain.common.exception.UnprocessableEntityException;
 import kr.hhplus.be.server.domain.concert.entity.Concert;
 import kr.hhplus.be.server.domain.concert.entity.ConcertSchedule;
 import kr.hhplus.be.server.domain.concert.entity.Seat;
@@ -20,7 +21,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -28,11 +28,11 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Transactional
 class ReservationConcurrencyTest {
     @Autowired
     ReservationUseCase reservationUseCase;
@@ -72,19 +72,25 @@ class ReservationConcurrencyTest {
         ConcertSchedule concertSchedule = concertScheduleJpaRepository.save(ConcertSchedule.create(concert, LocalDateTime.now().plusDays(1), 50));
         Seat seat = seatJpaRepository.save(Seat.create(concertSchedule, "1", true, 1000, LocalDateTime.now().plusMinutes(5)));
         seatId = seat.getId();
+
         List<User> users = new ArrayList<>();
         for (int i = 0; i < threadCount; i++) {
             User user = User.create("test_user");
-            userIds.add(user.getId());
             users.add(user);
         }
-        userJpaRepository.saveAll(users);
+        userJpaRepository.saveAllAndFlush(users);
+
+        for (User user : users) {
+            userIds.add(user.getId());
+        }
     }
 
     @AfterEach
     void tearDown() {
         reservationJpaRepository.deleteAll();
         seatJpaRepository.deleteAll();
+        concertScheduleJpaRepository.deleteAll();
+        concertJpaRepository.deleteAll();
         userJpaRepository.deleteAll();
     }
 
@@ -95,7 +101,8 @@ class ReservationConcurrencyTest {
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         CountDownLatch latch = new CountDownLatch(threadCount);
 
-        List<Boolean> results = new ArrayList<>(threadCount);
+        AtomicInteger successCnt = new AtomicInteger();
+        AtomicInteger failCnt = new AtomicInteger();
         List<Exception> exceptions = new ArrayList<>();
 
         // when
@@ -103,10 +110,10 @@ class ReservationConcurrencyTest {
             executorService.submit(() -> {
                 try {
                     reservationUseCase.makeTempReservation(userId, seatId);
-                    results.add(true);
+                    successCnt.getAndIncrement();
                 } catch (Exception e) {
                     exceptions.add(e);
-                    results.add(false);
+                    failCnt.getAndIncrement();
                 } finally {
                     latch.countDown();
                 }
@@ -116,11 +123,14 @@ class ReservationConcurrencyTest {
         executorService.shutdown();
 
         // then
-        long successCount = results.stream().filter(Boolean::booleanValue).count();
-        long failCount = results.stream().filter(result -> !result).count();
+        assertThat(successCnt.get()).isEqualTo(1);
+        assertThat(failCnt.get()).isEqualTo(threadCount - 1);
 
-        assertThat(successCount).isEqualTo(1);
-        assertThat(failCount).isEqualTo(threadCount - 1);
+        List<Reservation> reservations = reservationJpaRepository.findAll();
+        assertThat(reservations.size()).isEqualTo(1);
 
+        exceptions.stream().forEach(e -> {
+            assertThat(e).isInstanceOf(UnprocessableEntityException.class);
+        });
     }
 }
